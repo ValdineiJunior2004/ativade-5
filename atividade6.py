c8b5de535a6a125b796774f995cdd37507df247b
valdinei2004 <tad-valdineilima@ucpparana.edu.br>
2023-04-10 21:58:55 -0300

    xota

--- atividade6.py
+++ atividade6.py
@@ -6,9 +6,8 @@ for i in range(3):
     nota = float(input("Digite a nota do aluno: "))
     alunos[nome] = nota
 
-# calcula a média das notas
 soma_notas = sum(alunos.values())
 media_notas = soma_notas / len(alunos)
 
-# exibe a média
+
 print("A média das notas é:", media_notas)
---
+++ atividade7.py
@@ -0,0 +1,15 @@
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+soma = 0
+
+for linha in matriz:
+    for elemento in linha:
+        soma += elemento
+
+
+print("A soma de todos os elementos da matriz é:", soma)
